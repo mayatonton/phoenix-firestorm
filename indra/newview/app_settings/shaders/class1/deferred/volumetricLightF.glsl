bf269b8671f679697a858af1cf651077e2982ede
mayatonton <mayatonton1994@gmail.com>
2026-05-18 14:00:40 +0900

    r30 P3 step 1: import volumetric light shaders from Black Dragon Viewer
    BD 995a1354d8 から 2 ファイル取り込み (LGPL-2.1-only):
    - class1/deferred/volumetricLightF.glsl (stub, passthrough)
    - class3/deferred/volumetricLightF.glsl (godrays + shadow 積算本体)
    
    VS は LL 既存 deferred/postDeferredNoTCV.glsl を流用 (新規取り込みなし)。
    provenance コメントは P2 形式踏襲、BD バイト一致 + extra blank line 正規化のみ。

---
+++ indra/newview/app_settings/shaders/class1/deferred/volumetricLightF.glsl
@@ -0,0 +1,43 @@
+/**
+ * @file volumetricLightF.glsl
+ *
+ * $LicenseInfo:firstyear=2007&license=viewerlgpl$
+ * Second Life Viewer Source Code
+ * Copyright (C) 2007, Linden Research, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License only.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Linden Research, Inc., 945 Battery Street, San Francisco, CA  94111  USA
+ * $/LicenseInfo$
+ */
+
+// AYAstorm r30 P3: imported from BlackDragon Viewer (NiranV Dean), 995a1354d8, 2026-04-19
+// Source: https://github.com/NiranV/Black-Dragon-Viewer @ indra/newview/app_settings/shaders/class1/deferred/volumetricLightF.glsl
+// License: LGPL-2.1-only (same as Second Life Viewer Source Code, no relicensing)
+
+/*[EXTRA_CODE_HERE]*/
+
+out vec4 frag_color;
+
+uniform sampler2D diffuseRect;
+
+in vec2 vary_fragcoord;
+
+void main()
+{
+    vec2 tc = vary_fragcoord.xy;
+    vec4 diff = texture2D(diffuseRect, tc);
+    frag_color = diff;
+}
